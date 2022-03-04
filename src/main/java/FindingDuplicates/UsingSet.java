ebe86c61242173d486962d957d4ae734ac0ba78e
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-03-04 22:09:27 +0300

    Using set

--- src/main/java/FindingDuplicates/UsingSet.java
+++ src/main/java/FindingDuplicates/UsingSet.java
@@ -1,4 +1,18 @@
 package FindingDuplicates;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UsingSet {
+    public static void main (String [] args){
+        System.out.println("Duplicate arrays");
+        int [] nums = {1, 2, 3, 4, 2, 1};
+        Set<Integer> StoreElements = new HashSet<>();
+
+        for(int num: nums){
+            if(!StoreElements.add(num)){
+                System.out.println("Found duplicate: " + num);
+            }
+        }
+    }
 }
