95587ac4c6cfb6d4fbd2fb03c24424da4fe5523c
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-03-30 16:15:14 +0300

    finding unique characters in a string

---
+++ src/main/java/FindUniqueCharacters/UsingBruteForce.java
@@ -0,0 +1,33 @@
+package FindUniqueCharacters;
+
+public class UsingBruteForce {
+    /*===============================================================*/
+    /*=============METHOD TO FIND UNIQUE CHARACTERS==================*/
+    /*===============================================================*/
+    public static boolean findUniqueCharacters(String testString){
+        for(int i = 0; i < testString.length(); i++){
+            for(int j = i + 1; j < testString.length(); j++){
+                if(testString.charAt(i) == testString.charAt(j)){
+                    return  false;
+                }
+            }
+        }
+        return  true;
+
+    }
+    /*================================================================*/
+    /*===========================MAIN METHOD==========================*/
+    /*================================================================*/
+    public static void main (String [] args) {
+        String testString = "Mynameismarvin";
+        String newTestString = testString.toLowerCase();
+        if (findUniqueCharacters(newTestString)){
+            System.out.println("The string "+ testString + " has unique characters");
+        }
+
+        else {
+            System.out.println("The String " + testString + " do not have unique characters");
+        }
+    }
+}
+
