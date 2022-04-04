dd21dad025fbdd9e8e9d39909a3d13f6f1f97aee
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-04-04 12:56:16 +0300

    roman to integers

---
+++ src/main/java/RomanToInteger.java
@@ -0,0 +1,48 @@
+public class RomanToInteger {
+    /*Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M*/
+    /*  Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
+     */
+
+
+
+    /*====================================================================*/
+    /*========FUNCTION TO CHANGE ROMAN CHARACTERS TO INTEGERS=============*/
+    /*====================================================================*/
+
+    public static int changeToIntegers(String S){
+        int ans = 0;   int num = 0;
+
+        for (int i = S.length() - 1; i >= 0; i--){
+            switch (S.charAt(i)){
+                case 'I' : num = 1; break;
+                case 'V' : num = 5; break;
+                case 'X' : num = 10; break;
+                case 'L' : num = 50; break;
+                case 'C' : num = 100; break;
+                case 'D' : num = 500; break;
+                case 'M' : num = 1000; break;
+            }
+            if(4 * num < ans) ans -= num;
+            else {
+                ans += num;
+            }
+        }
+        return ans;
+
+    }
+
+    /*=====================================================================*/
+    /*=============================MAIN METHOD=============================*/
+    /*=====================================================================*/
+    public static void main (String [] args){
+      System.out.print(changeToIntegers("MCMXCIV"));
+      }
+    }
+
