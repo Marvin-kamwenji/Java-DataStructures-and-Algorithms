69402e3e55d7037a026df9ddca50dcba20aebf91
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-04-05 11:09:37 +0300

    longest common prefix

---
+++ src/main/java/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+public class LongestCommonPrefix {
+
+    /*====================================================================*/
+    /*================FUNCTION TO FIND LONGEST COMMON PREFIX==============*/
+    /*====================================================================*/
+    public static String findLongestCommonPrefix (String [] firstString){
+        if (firstString.length == 0 ){
+            return "";
+        }
+
+        String prefix = firstString[0];
+        for(int i = 1; i < firstString.length; i++){
+            while(firstString[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+    /*====================================================================*/
+    /*===============================MAIN METHOD==========================*/
+    /*====================================================================*/
+    public static void main (String [] args){
+      String [] firstString = {"flower", "flow", "flight"};
+
+      System.out.println(findLongestCommonPrefix(firstString));
+    }
+}
